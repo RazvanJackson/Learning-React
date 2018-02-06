0cb69d24a3f3ae7abde04d27886ffd54b5cac2d5
RazvanJackson <razvan_jackson@yahoo.com>
2018-02-06 17:18:01 +0200

    new updates

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import {Route, Router} from 'react-router-dom'
-import {ConnectedRouter} from 'react-router-redux'
-import createHistory from "history/createBrowserHistory"
-import thunk from 'redux-thunk'
-import allReducers from './reducers/index'
 
-import App from './App'
-import NotFound from './pages/NotFound'
+import ShoppingList from './ShoppingList'
 
-const store = createStore(allReducers, applyMiddleware(thunk))
-const history = createHistory();
-
-ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-          <Route path="/" component={App}>
-            <Route path="/about" component={NotFound}></Route>
-          </Route>
-        </Router>
-    </Provider>
-    ,
-    document.querySelector('#root')
-)
+ReactDOM.render(<ShoppingList/>, document.querySelector('#root'));
